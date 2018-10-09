ad627ace34ec2106d916ad8587354550ca06395e
anjaneyadatla <anjaneyalndatla@gmail.com>
2018-10-08 20:37:08 -0400

    Initial Commit

--- src/main/java/com/srkr/accounts/domain/model/postgres/TransactionsMapper.java
+++ src/main/java/com/srkr/accounts/domain/model/postgres/TransactionsMapper.java
@@ -9,6 +9,9 @@ public class TransactionsMapper {
 	@Autowired
 	private AccountsMapper accountsMapper;
 
+	@Autowired
+	private HeadersMapper headersMapper;
+
 	public Transactions toPostgresObject(com.srkr.accounts.domain.model.Transactions transactions) {
 		Transactions pgTransactions = new Transactions();
 		pgTransactions.setUserId(transactions.user_id());
@@ -16,7 +19,18 @@ public class TransactionsMapper {
 		pgTransactions.setLine_item_number(transactions.line_item_number());
 		pgTransactions.setName(transactions.name());
 		pgTransactions.setAccounts(accountsMapper.toPostgresObject(transactions.accounts()));
+		pgTransactions.setHeaders(headersMapper.toPostgresObject(transactions.header()));
 		return pgTransactions;
+	}
+
+	public com.srkr.accounts.domain.model.Transactions toDomainObject(Transactions pgTransactions) {
+		com.srkr.accounts.domain.model.Transactions transactions = new com.srkr.accounts.domain.model.Transactions(
+				pgTransactions.getId(), pgTransactions.getUserId(), pgTransactions.getUserName(),
+				pgTransactions.getLine_item_number(), pgTransactions.getName(),
+				accountsMapper.toDomainObject(pgTransactions.getAccounts()), pgTransactions.getQuantity(),
+				pgTransactions.getPrice(), pgTransactions.getAmount(),
+				headersMapper.toDomainObject(pgTransactions.getHeaders()));
+		return transactions;
 	}
 
 }
