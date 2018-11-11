9be2869a23dd7223304321504a8ee1a0bbcde1d6
anjaneyadatla <anjaneyalndatla@gmail.com>
2018-11-11 18:10:09 -0500

    changing the jenkins files stage order

--- src/main/java/com/srkr/accounts/config/WebSecurityConfig.java
+++ src/main/java/com/srkr/accounts/config/WebSecurityConfig.java
@@ -1,14 +1,16 @@
 package com.srkr.accounts.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-//@EnableWebSecurity
-public class WebSecurityConfig {
-	//extends WebSecurityConfigurerAdapter {
-//}
-	/*@Override
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
-	}*/
+		http.authorizeRequests().antMatchers("/*").permitAll().and().csrf().disable();
+	}
 }
+
